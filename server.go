2d473305f21a630310e1a621091f08218c7dfbb5
Radu Ioan Fericean <radu@fericean.ro>
2024-02-14 18:40:57 +0200

    first commit

---
+++ server.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"html/template"
+	"io"
+
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type User struct {
+	ID        string
+	FirstName string
+	Email     string
+}
+
+type Templates struct {
+	*template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	ctx := context.Background()
+	db, err := pgxpool.New(ctx,
+		"postgresql://sellerportal:6r74qFrOkHfYtFdR@172.17.0.1:5432/sellerportal")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	var users []*User
+	if err := pgxscan.Select(ctx, db, &users, `SELECT id, first_name, email FROM sp.user`); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to fetch: %v\n", err)
+	}
+
+	for _, u := range users {
+		fmt.Println(u)
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Renderer = &Templates{template.Must(template.ParseGlob("views/*.html"))}
+
+	e.GET("/link1", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "main", map[string]interface{}{"Name": "Link1"})
+	})
+	e.GET("/link2", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "main", map[string]interface{}{"Name": "Link2"})
+	})
+	e.GET("/link3", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "main", map[string]interface{}{"Name": "Link3"})
+	})
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", map[string]interface{}{"Name": "Home"})
+	})
+	e.Logger.Fatal(e.Start(":8000"))
+}
